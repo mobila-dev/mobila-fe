4cb33cc5fab761016eb60e178ed80a6048f304c8
AndreiBacila <dey.bacila09@yahoo.com>
2025-05-01 13:42:04 +0300

    added skeleton loader component

--- src/app/pages/home/sections/landing-section/landing-section.component.ts
+++ src/app/pages/home/sections/landing-section/landing-section.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SkeletonLoaderComponent } from '../../../../components/skeleton-loader/skeleton-loader.component';
 
 @Component({
   selector: 'app-landing-section',
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [NgOptimizedImage, RouterLink, SkeletonLoaderComponent],
   templateUrl: './landing-section.component.html',
   styleUrl: './landing-section.component.scss',
 })

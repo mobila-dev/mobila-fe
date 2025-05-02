cf5656e05d979ae62b6e5bf054da23621d6001c9
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2025-05-02 12:44:26 +0300

    Shop by room base structure & css (#8)
    * base structure
    
    * added nav arrow

--- src/app/components/mock/mock.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-mock',
-  imports: [],
-  templateUrl: './mock.component.html',
-  styleUrl: './mock.component.scss',
-})
-export class MockComponent {} // Mock component for setting up the folder structure
--- src/app/pages/home/sections/by-room-section/by-room-section.component.ts
+++ src/app/pages/home/sections/by-room-section/by-room-section.component.ts
@@ -1,9 +1,18 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-by-room-section',
-  imports: [],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './by-room-section.component.html',
   styleUrl: './by-room-section.component.scss',
 })
-export class ByRoomSectionComponent {}
+export class ByRoomSectionComponent {
+  activeImage: 'living-room' | 'bedroom' | 'kitchen' | 'hallway' =
+    'living-room';
+
+  setActiveImage(image: 'living-room' | 'bedroom' | 'kitchen' | 'hallway') {
+    this.activeImage = image;
+  }
+}

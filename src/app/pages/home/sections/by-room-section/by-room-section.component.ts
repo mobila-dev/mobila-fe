2744958f209b5098fc89355bdf0dc5bac36b7e44
AndreiBacila <dey.bacila09@yahoo.com>
2025-05-02 12:11:08 +0300

    base structure

--- src/app/pages/home/sections/by-room-section/by-room-section.component.ts
+++ src/app/pages/home/sections/by-room-section/by-room-section.component.ts
@@ -1,9 +1,17 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-by-room-section',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './by-room-section.component.html',
   styleUrl: './by-room-section.component.scss',
 })
-export class ByRoomSectionComponent {}
+export class ByRoomSectionComponent {
+  activeImage: 'living-room' | 'bedroom' | 'kitchen' | 'hallway' =
+    'living-room';
+
+  setActiveImage(image: 'living-room' | 'bedroom' | 'kitchen' | 'hallway') {
+    this.activeImage = image;
+  }
+}

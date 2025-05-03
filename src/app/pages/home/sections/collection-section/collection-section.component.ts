eab850367060fb416c1a18bd4e6d902cd7ad29a3
Tasadan Luca <luca.tasadan@gmail.com>
2025-05-04 01:08:34 +0300

    Collection section done

--- src/app/pages/home/sections/collection-section/collection-section.component.ts
+++ src/app/pages/home/sections/collection-section/collection-section.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+
 @Component({
   selector: 'app-collection-section',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './collection-section.component.html',
   styleUrl: './collection-section.component.scss',
 })
